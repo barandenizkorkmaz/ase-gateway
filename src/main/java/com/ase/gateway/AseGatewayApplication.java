c8c1c8c8c195382202ae5d65e53ad7cb070118a9
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2022-12-26 11:11:06 +0300

    Gateway init

---
+++ src/main/java/com/ase/gateway/AseGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ase.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AseGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AseGatewayApplication.class, args);
+	}
+}
